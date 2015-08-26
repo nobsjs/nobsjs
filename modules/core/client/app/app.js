b581dd8fef7e7e5d0b090a47f6728c2dca87dd47
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-26 10:27:41 -0700

    [feat] initializes angular application

---
+++ modules/core/client/app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var tropicalbs = angular.module('tropicalbs', ['ui.router']);
+
+tropicalbs.config(function ($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: '../../../../modules/core/client/views/core.client.home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
