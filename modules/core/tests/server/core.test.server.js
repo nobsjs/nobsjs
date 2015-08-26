109d1801693151424f83d4fdd1f50b725171685c
Rob Rodrigez <robrod@gmail.com>
2015-08-25 19:55:01 -0700

    Server Test framework

---
+++ modules/core/tests/server/core.test.server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+describe('"Hello World"', function() {
+
+  it('should respond to a root request with "Hello World"', function() {
+    
+    expect(true).toEqual(true);
+
+  });
+  
+});
