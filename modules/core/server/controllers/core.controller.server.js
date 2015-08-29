7b784c63f134842a7ba3d3cc85a7b221e3421438
Rob Rodrigez <robrod@gmail.com>
2015-08-29 15:22:26 -0700

    [feat] Implement checkAuth and decode

--- modules/core/server/controllers/core.controller.server.js
+++ modules/core/server/controllers/core.controller.server.js
@@ -64,3 +64,47 @@ exports.signUp = function (req, res) {
     }
   });
 };
+
+exports.checkAuth = function (req, res, next) {
+  // checking to see if the user is authenticated
+  // grab the token in the header is any
+  // then decode the token, which we end up being the user object
+  // check to see if that user exists in the database
+  var token = req.headers['x-access-token'];
+  if (!token) {
+    next(new Error('No token'));
+  } else {
+    var user = jwt.decode(token, config.secret);
+    User.findOne({email: user.email})
+      .then(function (foundUser) {
+        if (foundUser) {
+          res.send(200);
+        } else {
+          res.status(401).send('User does not exist');
+        }
+      })
+      .catch(function (e) {
+        next(e);
+      });
+  }
+};
+
+exports.decode = function(req, res, next){
+  var token = req.headers['x-access-token'];
+  var user;
+
+  if (!token) {
+    return res.send(403); // send forbidden if a token is not provided
+  }
+
+  try {
+    // decode token and attach user to the request
+    // for use inside our controllers
+    user = jwt.decode(token, config.secret);
+    req.user = user;
+    next();
+  } catch(error) {
+    return next(error);
+  }
+
+};
