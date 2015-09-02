b3a93a94852de0236bccec4cb884d0ecafd56a0d
Rob Rodrigez <robrod@gmail.com>
2015-09-02 16:17:33 -0700

    [refactor] refactor app.js according to style guide

---
+++ modules/core/client/routes/app.routes.client.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('tropicalbs')
+.config(mainConfig);
+
+mainConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider'];
+
+function mainConfig ($stateProvider, $urlRouterProvider, $httpProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    // MAIN PARENT ROUTE ========
+    .state('app', {
+      templateUrl: '../../../../modules/core/client/views/app.view.client.html',
+      abstract: true,
+      controller: 'AppController'
+    });
+
+  $httpProvider.interceptors.push('AttachTokens');
+}
+
+
+
+
+
