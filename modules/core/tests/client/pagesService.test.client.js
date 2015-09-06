1d13bfa224cd1aa686814b2469a2861ee3b517aa
Marco Viappiani <marco.viappiani@gmail.com>
2015-09-05 17:19:48 -0700

    [refactor] corrects spacing for style guide in tests

--- modules/core/tests/client/pagesService.test.client.js
+++ modules/core/tests/client/pagesService.test.client.js
@@ -3,7 +3,7 @@
 describe('pagesService', function () {
   var $http, $state, pagesService, $httpBackend, pageObject;
 
-  beforeEach(function() {
+  beforeEach(function () {
 
     module('tropicalbs');
 
@@ -22,72 +22,72 @@ describe('pagesService', function () {
     };
   });
 
-  describe('Page Create', function() {
-    it('should have a create page function', function() {
+  describe('Page Create', function () {
+    it('should have a create page function', function () {
       expect(typeof pagesService.createPage).toEqual('function');
     });
 
-    it('should be able to create a page', function() {
+    it('should be able to create a page', function () {
       $httpBackend.expectPOST('/api/core/pages').respond(pageObject);
-      pagesService.createPage(pageObject).then(function(res) {
+      pagesService.createPage(pageObject).then(function (res) {
         expect(res).toEqual(pageObject);
       });
     });
   });
 
-  describe('Page Delete', function() {
-    it('should have a delete page function', function() {
+  describe('Page Delete', function () {
+    it('should have a delete page function', function () {
       expect(typeof pagesService.deletePage).toEqual('function');
     });
-    it('should be able to delete a page', function() {
+    it('should be able to delete a page', function () {
       $httpBackend.expectDELETE('/api/core/pages' + pageObject.id).respond(pageObject);
-      pagesService.deletePage(pageObject).then(function(res) {
+      pagesService.deletePage(pageObject).then(function (res) {
         expect(res).toEqual(pageObject);
       });
     });
   });
 
 
-  describe('Get All Pages', function() {
-    it('should have a getAllPages function', function() {
+  describe('Get All Pages', function () {
+    it('should have a getAllPages function', function () {
       expect(typeof pagesService.getAllPages).toEqual('function');
     });
-    it('should be able to get all Pages', function() {
+    it('should be able to get all Pages', function () {
       $httpBackend.expectGET('/api/core/pages').respond([pageObject,pageObject]);
-      pagesService.getAllPages(pageObject).then(function(res) {
+      pagesService.getAllPages(pageObject).then(function (res) {
         expect(res).toEqual([pageObject,pageObject]);
       });
     });
   });
 
 
-  describe('Get One Page', function() {
-    it('should have a getPage property', function() {
+  describe('Get One Page', function () {
+    it('should have a getPage property', function () {
       expect(typeof pagesService.getPage).toEqual('function');
     });
-    it('should be able to get a page', function() {
+    it('should be able to get a page', function () {
       $httpBackend.expectPOST('/api/core/pages/' + pageObject.id).respond(pageObject);
-      pagesService.getPage(pageObject).then(function(res) {
+      pagesService.getPage(pageObject).then(function (res) {
         expect(res).toEqual(pageObject);
       });
     });
   });
 
 
-  describe('Update Page', function() {
-    it('should have a updatePage function', function() {
+  describe('Update Page', function () {
+    it('should have a updatePage function', function () {
       expect(typeof pagesService.updatePage).toEqual('function');
     });
-    it('should be able to update a page', function() {
+    it('should be able to update a page', function () {
       $httpBackend.expectPUT('/api/core/pages/' + pageObject.id).respond(pageObject);
-      pagesService.updatePage(pageObject).then(function(res) {
+      pagesService.updatePage(pageObject).then(function (res) {
         expect(res).toEqual(pageObject);
       });
     });
   });
 
-  describe('Current State', function() {
-    it('should have a property to return the current state', function() {
+  describe('Current State', function () {
+    it('should have a property to return the current state', function () {
       expect(pagesService.currentState).toEqual($state.current.name);
     });
   });
