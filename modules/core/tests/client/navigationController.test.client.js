62a1582655d737416e531cb25390e80e1182d2cf
Marco Viappiani <marco.viappiani@gmail.com>
2015-09-07 15:57:43 -0700

    [test] Client-side controller tests (admin, navigation, pages)

--- modules/core/tests/client/navigationController.test.client.js
+++ modules/core/tests/client/navigationController.test.client.js
@@ -1,17 +1,122 @@
+/* globals spyOn */
 'use strict';
 
 describe('NavigationController', function () {
 
-  var navigationController, state;
+  var $state, authService, navService, userService, vm, createController, $controller;
 
-  beforeEach(module('tropicalbs'));
+  beforeEach(function () {
 
-  beforeEach(inject(function ($controller, $state) {
-    navigationController = $controller('NavigationController');
-    state = $state;
-  }));
+    module('tropicalbs');
 
-  it('should exist', function () {
-    expect(navigationController).not.toBeUndefined();
+    inject(function ($injector) {
+      $controller = $injector.get('$controller');
+      $state = $injector.get('$state');
+      authService = $injector.get('authService');
+      navService = $injector.get('navService');
+      userService = $injector.get('userService');
+    });
+
+    createController = function () {
+      return $controller('NavigationController', {
+        $state: $state,
+        authService: authService,
+        navService: navService,
+        userService: userService
+      });
+    };
+    vm = createController();
+  });
+
+  it('NavigationController should exist', function () {
+    expect(vm).toBeDefined();
+  });
+
+  describe('view model', function () {
+
+    it('vm should have a state property', function () {
+      expect(vm.state).toBeDefined();
+    });
+
+    it('vm should have a tabs property', function () {
+      expect(vm.tabs).toBeDefined();
+    });
+
+    it('vm should have a user property', function () {
+      expect(vm.user).toBeDefined();
+    });
+
+    it('vm should have a login property', function () {
+      expect(vm.login).toBeDefined();
+    });
+
+    it('vm should have a signup property', function () {
+      expect(vm.signup).toBeDefined();
+    });
+
+  });
+
+  describe('tabs', function () {
+
+    it('tabs should be an array', function () {
+      expect(Array.isArray(vm.tabs)).toBe(true);
+    });
+
+  });
+
+  describe('login', function () {
+
+    it('login should be a function', function () {
+      expect(typeof vm.login).toEqual('function');
+    });
+
+    it('should move to the login page after launching login', function () {
+      spyOn($state, 'go');
+      vm.login();
+      expect($state.go).toHaveBeenCalledWith('login');
+    });
+
+  });
+
+  describe('logout', function () {
+
+    it('logout should be a function', function () {
+      expect(typeof vm.logout).toEqual('function');
+    });
+
+  });
+
+  describe('signup', function () {
+
+    it('signup should be a function', function () {
+      expect(typeof vm.signup).toEqual('function');
+    });
+
+    it('should move to the signup page after launching signup', function () {
+      spyOn($state, 'go');
+      vm.signup();
+      expect($state.go).toHaveBeenCalledWith('signup');
+    });
+
+  });
+
+  describe('user', function () {
+
+    it('user should be a function', function () {
+      expect(typeof vm.user).toEqual('object');
+    });
+
+    it('user should have an email property', function () {
+      expect(vm.user.email).toBeDefined();
+    });
+
+    it('user should have roles', function () {
+      expect(vm.user.roles).toBeDefined();
+    });
+
+    it('roles should be an array', function () {
+      expect(Array.isArray(vm.user.roles)).toBe(true);
+    });
+
   });
 });
--- modules/core/tests/client/pagesController.test.client.js
+++ modules/core/tests/client/pagesController.test.client.js
@@ -2,15 +2,46 @@
 
 describe('PagesController', function () {
 
-  var pagesController;
+  var $controller, $state, pagesService, createController, vm;
 
-  beforeEach(module('tropicalbs'));
+  beforeEach(function () {
 
-  beforeEach(inject(function ($controller) {
-    pagesController = $controller('PagesController');
-  }));
+    module('tropicalbs');
+
+    inject(function ($injector) {
+      $controller = $injector.get('$controller');
+      $state = $injector.get('$state');
+      pagesService = $injector.get('pagesService');
+    });
+
+    createController = function () {
+      return $controller('PagesController', {
+        $state: $state,
+        pagesService: pagesService
+      });
+    };
+    vm = createController();
+  });
+
 
   it('should exist', function () {
-    expect(pagesController).not.toBeUndefined();
+    expect(vm).toBeDefined();
   });
+
+  xit('should have a title', function () {
+    expect(vm.title).toBeDefined();
+  });
+
+  xit('title should be a string', function () {
+    expect(typeof vm.title).toEqual('string');
+  });
+
+  xit('should have content', function () {
+    expect(vm.content).toBeDefined();
+  });
+
+  xit('content should be a string', function () {
+    expect(typeof vm.content).toEqual('string');
+  });
+
 });
