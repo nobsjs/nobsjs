dcfb423e53c8249722af2417f5be68cc9d5e9ef2
Cody B. Daig <cody@daig.me>
2015-09-08 16:51:13 -0700

    [feat][test] Posts Service

---
+++ modules/blog/client/services/blog.service.client.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('tropicalbs')
+  .factory('blogService', blogService);
+
+blogService.$inject = ['$http', '$state'];
+
+function blogService ($http, $state) {
+  var blog = {
+    createPost: createPost,
+    deletePost: deletePost,
+    getAllPosts: getAllPosts,
+    getPost: getPost,
+    updatePost: updatePost
+  };
+
+  return blog;
+
+  //////////
+
+  function createPost (post) {
+    var req = {
+      method: 'POST',
+      url: 'api/blog/posts',
+      data: post
+    };
+
+    return $http(req)
+      .then(returnData);
+      // no catch block allows the controller to receive the error and provide feedback to the user
+  }
+
+  function deletePost (postId) {
+    var req = {
+      method: 'DELETE',
+      url: '/api/blog/posts/' + postId
+    };
+
+    return $http(req)
+      .then(redirectHome);
+      // no catch block allows the controller to receive the error and provide feedback to the user
+  }
+
+  function getAllPosts () {
+    var req = {
+      method: 'GET',
+      url: '/api/blog/posts'
+    };
+
+    return $http(req)
+      .then(returnData);
+      // no catch block allows the controller to receive the error and provide feedback to the user
+  }
+
+  function getPost (postId) {
+    var req = {
+      method: 'GET',
+      url: 'api/blog/posts/' + postId
+    };
+
+    return $http (req)
+      .then(returnData);
+      // no catch block allows the controller to receive the error and provide feedback to the user
+  }
+
+  function redirectHome () {
+    $state.go('home');
+  }
+
+  function returnData (res) {
+    return res.data;
+  }
+
+  function updatePost (post, postId) {
+    var req = {
+      method: 'PUT',
+      url: '/api/blog/posts/' + postId,
+      data: post
+    };
+
+    return $http(req)
+      .then(returnData)
+      .then(redirectHome);
+      // no catch block allows the controller to receive the error and provide feedback to the user
+  }
+}
---
+++ modules/blog/tests/client/blog.service.test.client.js
@@ -0,0 +1,115 @@
+'use strict';
+
+describe('blogService', function () {
+
+  var $http, $httpBackend, $state, postObject, blogService;
+
+  beforeEach(function () {
+
+    module('tropicalbs');
+
+    inject(function ($injector) {
+      $http = $injector.get('$http');
+      $httpBackend = $injector.get('$httpBackend');
+      $state = $injector.get('$state');
+
+      blogService = $injector.get('blogService');
+    });
+
+    postObject = {
+      content: 'Content for the Blog Post',
+      title: 'Blog Post Title'
+    };
+
+  });
+
+  describe('Create Post', function () {
+
+    it('should have a create post function', function () {
+      expect(typeof blogService.createPost).toEqual('function');
+    });
+
+    it('should be able to create a post', function () {
+      $httpBackend.expectPOST('/api/blog/posts').respond(postObject);
+      blogService.createPost(postObject)
+        .then(function (res) {
+          expect(res).toEqual(postObject);
+        });
+    });
+
+  });
+
+  describe('Delete Post', function () {
+
+    it('should have a delete post function', function () {
+      expect(typeof blogService.deletePost).toEqual('function');
+    });
+
+    it('should be able to delete a post', function () {
+      postObject.id = 7;
+
+      $httpBackend.expectDELETE('/api/core/posts' + postObject.id).respond(postObject);
+
+      blogService.deletePost(postObject).then(function (res) {
+        expect(res).toEqual(postObject);
+      });
+    });
+
+  });
+
+  describe('Update Post', function () {
+
+    it('should have a updatePost function', function () {
+      expect(typeof blogService.updatePost).toEqual('function');
+    });
+
+    it('should be able to update a post', function () {
+      postObject.id = 9;
+      $httpBackend.expectPUT('/api/core/posts/' + postObject.id).respond(postObject);
+      blogService.updatePost(postObject, postObject.id).then(function (res) {
+        expect(res).toEqual(postObject);
+      });
+    });
+
+  });
+
+  describe('Get All Posts', function () {
+
+    it('should have a get All posts function', function () {
+      expect(typeof blogService.getAllPosts).toEqual('function');
+    });
+
+    it('should be able to get all posts', function () {
+      postObject.id = 19;
+      var postObject2 = {
+        content: 'Some content for the second post',
+        id: 56,
+        title: 'The title for post 2'
+      };
+
+      $httpBackend.expectGET('/api/core/posts').respond([postObject, postObject2]);
+      blogService.getAllPosts().then(function (res) {
+        expect(res).toEqual([postObject, postObject2]);
+      });
+
+    });
+
+  });
+
+  describe('Get Post', function () {
+
+    it('should have a get post function', function () {
+      expect(typeof blogService.getPost).toEqual('function');
+    });
+
+    it('should be able to get a post', function () {
+      postObject.id = 98;
+      $httpBackend.expectGET('/api/core/posts/' + postObject.id).respond(postObject);
+      blogService.getPost(postObject).then(function (res) {
+        expect(res).toEqual(postObject);
+      });
+    });
+
+  });
+
+});
